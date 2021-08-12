6ddf983e834e73ae404ad6cb21e7520bda1b9890
Fredrik Simonsson <simson@polkadex.trade>
2021-08-12 15:07:14 +0900

    polkadexinternaltestnet

--- src/main.rs
+++ src/main.rs
@@ -20,22 +20,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .get_matches();
 
-    let url: String = "ws://127.0.0.1:9944".into();
+    let url: String = "wss://internaltestnet.polkadex.trade".into();
     let from = AccountKeyring::Alice.pair();
-    let api = Api::new(url).map(|api| api.set_signer(from)).unwrap();
+    let api = Api::new(url).map(|api| api.set_signer(from))?;
 
     // set the recipient
-    // let to = AccountKeyring::Bob.to_account_id();
+    let to = AccountKeyring::Bob.to_account_id();
 
     let stdin = std::io::stdin();
     let mut it = stdin.lock().lines();
+     #[allow(clippy::redundant_clone)]
 
-    // let mut next:Option<String> = Some("Hello")
+      // From RPC  Metadata
+      //             name: PolkadexIdo,
+      //calls: [
+      // {
+      //   name: register_investor,
+      //   args: [],
+      //   docs: [
+      //      Registers a new investor to allow participating in funding round.,
+      //     ,
+      //      # Parameters,
+      //     ,
+      //      * `origin`: Account to be registered as Investor
+      //   ]
+      // },
+      // {
 
-    if matches.is_present("hello") {
-        #[allow(clippy::redundant_clone)]
+
         let xt: UncheckedExtrinsicV4<_> =
-            compose_extrinsic!(api.clone(), "MSGModule", "do_something", "Hello Chain!!");
+            compose_extrinsic!(api.clone(), "PolkadexIdo", "register_investor", to);
 
         println!("[+] Composed Extrinsic:\n {:?}\n", xt);
 
@@ -44,53 +58,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         if let Some(tx_hash) = tx_hash {
             println!("[+] Transaction got included. Hash: {:?}", tx_hash);
         }
-        let fetchstorage = api.get_storage_value::<Vec<u8>>("MSGModule", "Something", None);
-
-        match fetchstorage {
-            Ok(Some(ans)) => {
-                println!("Something: {:?}", ans);
-                if let Ok(s) = String::from_utf8(ans) {
-                    println!("{}", s);
-                }
-            }
-            Ok(_) => {
-                println!("No value returned");
-            }
-            Err(e) => {
-                println!("Error from api {:?}", e);
-            }
-        }
-    } else {
-        while let Some(Ok(s1)) = it.next() {
-            #[allow(clippy::redundant_clone)]
-            let xt: UncheckedExtrinsicV4<_> =
-                compose_extrinsic!(api.clone(), "MSGModule", "do_something", s1);
-
-            println!("[+] Composed Extrinsic:\n {:?}\n", xt);
-
-            // send and watch extrinsic until InBlock
-            let tx_hash = api.send_extrinsic(xt.hex_encode(), XtStatus::InBlock)?;
-            if let Some(tx_hash) = tx_hash {
-                println!("[+] Transaction got included. Hash: {:?}", tx_hash);
-            }
-        }
-    }
-    if matches.is_present("fetch") {
-        let fetchstorage = api.get_storage_value::<U256>("MSGModule", "Something", None);
-
-        match fetchstorage {
-            Ok(Some(ans)) => {
-                println!("Something: {:?}", ans);
-                println!("Something: {:x}", ans);
-            }
-            Ok(_) => {
-                println!("No value returned");
-            }
-            Err(e) => {
-                println!("Error from api {:?}", e);
-            }
-        }
-    }
 
     Ok(())
 }
